d8553f92eaea971ba4929a02fd6877a5151f7bc9
Kk <143418966+Kkmgla@users.noreply.github.com>
2024-06-20 15:58:10 +0300

    Add files via upload

---
+++ 31.Next_Permutation/31. Next Permutation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(), nums.end());
+    }
+};
