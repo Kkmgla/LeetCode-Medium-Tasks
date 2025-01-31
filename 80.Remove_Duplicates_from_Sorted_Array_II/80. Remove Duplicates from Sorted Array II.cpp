154201e80a448295d78504a50b35c6af007ba1ba
Kk <143418966+Kkmgla@users.noreply.github.com>
2025-01-31 17:04:31 +0300

    Add files via upload

---
+++ 80.Remove_Duplicates_from_Sorted_Array_II/80. Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 2;
+
+        if (nums.size() <= 2) return nums.size();
+
+        for (int i = 2; i < nums.size(); i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+};
+
+
+int main()
+{
+    Solution s;
+    vector <int> nums = { 1, 2, 2, 3 };
+    cout << s.removeDuplicates(nums);
+}
